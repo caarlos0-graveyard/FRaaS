127d73c2f86b3341f3e8667b2f5c5f7db17b87bf
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-02-06 17:39:13 -0200

    way better now

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express'),
   app = express(),
+  mustacheExpress = require('mustache-express'),
   RTProvider = require('./lib/rt-provider').RTProvider,
   provider = new RTProvider()
 
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
 app.get('/:user', function (req, res) {
-  res.send(provider.random(req.params.user))
+  var user = req.params.user,
+    rt = provider.random(user)
+  res.render('rt', {user: user, rt: rt})
 })
 
 app.listen(process.env.PORT || 4567)
